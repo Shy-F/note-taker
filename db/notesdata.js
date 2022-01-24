c8451260e4d7e1de0282e72e969adf2808a31284
Shy <fleetwoodshybreasia@gmail.com>
2022-01-23 21:20:22 -0500

    write read get and add notes

---
+++ db/notesdata.js
@@ -0,0 +1,31 @@
+const util = require('util');
+const fs = require('fs');
+
+const readNote = util.promisify(fs.readFile);
+const writeNote = util.promisify(fs.writeFile);
+
+class Save {
+    write(note) {
+        return writeNote('db/db.json', JSON.stringify(note));
+    }
+
+    read() {
+        return readNote('db/db.json', 'utf-8');
+    }
+
+    getNotes() {
+        return this.read().then(notes => {
+            let parsedNotes;
+            try {
+                parsedNotes = [].concat(JSON.parse(notes));
+            } catch (err) {
+                parsedNotes = [];
+            }
+            return parsedNotes;
+        });
+    }
+
+    addNote(note) {
+        
+    }
+}
