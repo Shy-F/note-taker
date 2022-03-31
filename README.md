9ef648b11b22eea668b8aba9f95ba6fc45b0ef11
Shy <fleetwoodshybreasia@gmail.com>
2022-03-31 19:26:00 -0400

    updates

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Note Taker Starter Code
+# Note Taker 
+
+## Description
+This is a note taker that saves notes using ExpressJs
+
+## Installing
+Clone to your repo. run npm install to install depedendecies. run node server.js
+
+## Preview
--- server.js
+++ server.js
@@ -1,17 +1,16 @@
-const express = require('express');
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const express = require("express");
+const fs = require("fs");
+const path = require('path');
 
-const PORT = 3001;
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname));
 
-app.use(express.static('public'));
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+require('./routes/routes')(app);
 
-app.listen(PORT, () => {
-    console.log(`API server is ready on port ${PORT}!`);
-});
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});  
